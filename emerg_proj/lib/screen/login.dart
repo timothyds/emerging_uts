48f8dd6b660f8d479807e905711f37a68c679a76
Timothy Dewanto <dewantotims@gmail.com>
2024-04-30 14:34:28 +0700

    new

---
+++ emerg_proj/lib/screen/login.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:emerg_proj/main.dart';
+
+String _user_id="";
+String _password_id="";
+void doLogin() async {
+    //later, we use web service here to check the user id and password
+    final prefs = await SharedPreferences.getInstance();
+    
+    prefs.setString("user_id", _user_id);
+    prefs.setString("user_pass", _password_id);
+    main();
+  }
+
+
+class MyLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Login(),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginState();
+  }
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: Container(
+          height: 300,
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+              border: Border.all(width: 1),
+              color: Colors.white,
+              boxShadow: [BoxShadow(blurRadius: 20)]),
+          child: Column(children: [
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                onChanged: (value){
+                  _user_id = value;
+                },
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(
+                  height: 50,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20)),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      doLogin();
+                    },
+                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.greenAccent)),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                  ),
+                )),
+          ]),
+        ));
+  }
+}
+
